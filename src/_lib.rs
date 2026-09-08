1d9791b8c7b946ff1a93470c670066111d402cb8
MaulingMonkey <git@maulingmonkey.com>
2026-09-08 11:24:29 -0700

    allow(dead_code, unused_imports) crate-wide unless --all-features

--- src/_lib.rs
+++ src/_lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../Readme.md")]
 #![no_std]
 #![deny(non_snake_case, unreachable_patterns)] // catch typoed match ... { ERROR_... => ... }
+#![cfg_attr(not(all(feature = "std", feature = "winresult")), allow(dead_code, unused_imports))] // ignore many dead_code false positives without --all-features
 
 #[cfg(feature = "alloc")]   extern crate alloc;
 #[cfg(feature = "std")]     extern crate std;
