7d44d0c89e5bda651af1124dd2e051eea27c818c
MaulingMonkey <git@maulingmonkey.com>
2026-09-08 22:49:19 -0700

    tests\libc.rs: spam more c string literals, // SAFETY audit

--- tests/libc.rs
+++ tests/libc.rs
@@ -10,9 +10,9 @@ use core::fmt::{self, Debug, Formatter};
 
 #[allow(dead_code)] // Imported methods not actually used
 struct Example {
-    puts:               unsafe extern "C" fn (_: *const c_char) -> c_int,
-    invalid_optional:   Option<unsafe extern "C" fn (_: *const c_char) -> c_int>,
-    invalid_required:   unsafe extern "C" fn (_: *const c_char) -> c_int,
+    puts:               unsafe extern "C" fn (_: *const c_char) -> c_int,           // ✔️ correct signature
+    invalid_optional:   Option<unsafe extern "C" fn (_: *const c_char) -> c_int>,   // ⚠️ should never exist, and never used, so any signature "should" be OK
+    invalid_required:   unsafe extern "C" fn (_: *const c_char) -> c_int,           // ⚠️ should never exist, and never used, so any signature "should" be OK
 }
 
 impl Debug for Example {
@@ -27,6 +27,7 @@ impl Example {
     }
 
     pub fn from(lib: Library) -> Result<Self> {
+        // SAFETY: ⚠️ see per-member notes in example struct
         unsafe{Ok(Self{
             puts:               lib.sym(c"puts")?,
             invalid_optional:   lib.sym_opt(c"invalid_optional"),
@@ -44,6 +45,7 @@ impl Example {
 #[test] fn load_unload() {
     if !std::env::var_os("CI").is_some() {
         let lib = Library::load("/lib/x86_64-linux-gnu/libc.so.6").expect("loading libc.so.6");
+        // SAFETY: ✔️ not used in production, doesn't seem to crash from unloading libc.so.6 in testing on WSL either.
         unsafe { lib.close_unsafe_unsound_possible_noop_do_not_use_in_production() }.expect("unloading libc.so.6");
     }
 }
@@ -58,12 +60,15 @@ impl Example {
 }
 
 #[test] fn ok_sym() {
+    // SAFETY: ✔️ see bellow
     unsafe {
+        // ✔️ correct signature
         let puts : unsafe extern "C" fn (_: *const c_char) -> c_int
             = Library::load("/lib/x86_64-linux-gnu/libc.so.6").unwrap()
             .sym(c"puts").unwrap();
 
-        puts(b"Hello, world!\0".as_ptr() as _);
+        // ✔️ valid string, no other preconditions
+        puts(c"Hello, world!".as_ptr().cast());
     }
 }
 
